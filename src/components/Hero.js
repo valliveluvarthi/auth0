e80ef3be7a96067510d506ca819a21953d26841c
VELUVARTHI VALLI <simhavalli25397@gmail.com>
2023-03-22 17:38:22 +0530

    auth 0 concept added

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import logo from "../assets/logo.svg";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <h1 className="mb-4">React.js Auth POC</h1>
+
+    <p className="lead">
+      This is a sample application that demonstrates an authentication flow 
+    </p>
+  </div>
+);
+
+export default Hero;
